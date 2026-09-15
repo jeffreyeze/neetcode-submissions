8b6477343c03144c6309d2a9448a232f674de641
Jeffrey Eze <jeffreyeze012@gmail.com>
2026-09-15 22:22:43 +0100

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> count(26, 0);
+        if(s.size() != t.size()){
+            return false;
+        }
+        for(int i = 0; i < s.size(); i++){
+            count[s[i] - 'a']++;
+        }
+        for(int i = 0; i < t.size(); i++){
+            count[t[i] - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(count[i] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};

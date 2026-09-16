89e144980ae89c62584f042034e0ac2d73d33b1c
Jeffrey Eze <jeffreyeze012@gmail.com>
2026-09-16 10:08:56 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while(left < right){
+            while(left < s.size() && !isalnum(s[left])){
+                left++;
+            }
+            while(right >= 0 && !isalnum(s[right])){
+                right--;
+            }
+            if(left>= right){
+                break;
+            }
+            if(tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            else{
+                left++;
+                right--; 
+            }
+        }
+        return true;
+    }
+};
